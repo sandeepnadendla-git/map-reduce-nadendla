8826a42329646866c2193e750f08bad69a98c6f3
sandeepnadendla-git <77738760+sandeepnadendla-git@users.noreply.github.com>
2022-01-26 13:50:01 -0600

    Adding Python file

---
+++ 01mapper.py
@@ -0,0 +1,12 @@
+# Sandeep Nadendla
+# Example mapper
+
+f = open("d.txt","r")           # open file, read-only
+for line in f:  
+    dataList = line.strip().split("    ")    # count the spaces!
+    print (dataList   )
+    print (len(dataList  ))
+    if len(dataList  ) == 6:
+        date, time, location, dept, amount, payType = dataList  #assign names
+        print ("{0}\t{1}".format(location, amount))
+f.close()
